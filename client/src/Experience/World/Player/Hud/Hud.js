adaf1f1ef4ac85d459bac44770415708e6ffa718
Thomas Ghignon <thomas@everide.app>
2023-05-20 18:45:19 +0200

    💄 Add new quest UI

--- client/src/Experience/World/Player/Hud/Hud.js
+++ client/src/Experience/World/Player/Hud/Hud.js
@@ -10,7 +10,6 @@ export default class Hud {
 
     update() {
         this.chat.update()
-        this.QuestWindow.update()
     }
 
     destroy() {
--- client/src/Experience/World/Player/Hud/QuestWindow.js
+++ client/src/Experience/World/Player/Hud/QuestWindow.js
@@ -6,18 +6,78 @@ export default class QuestWindow extends EventEmitter{
     super();
     this.experience = new Experience()
 
+    this.quest = null
+
     this.init()
   }
 
-  init() {
-
+  async init() {
+    await this.wait(() => this.experience.world !== undefined).then(async () => {
+      await this.wait(() => this.experience.world.quest !== undefined).then(() => {
+        this.quest = this.experience.world.quest
+        this.updateUI()
+        this.experience.world.quest.on("update", () => {
+          console.log("UIQuest update")
+          this.quest = this.experience.world.quest
+          this.updateUI()
+        })
+      })
+    })
   }
 
-  update() {
+  updateUI() {
+    const questWindow = document.getElementById("quest")
+    questWindow.innerHTML = ""
+    let questList = ""
+    Object.entries(this.quest.activeQuests).forEach(([id, quest]) => {
+      questList += this.setUIQuest(quest)
+    });
+    questWindow.innerHTML = questList
+  }
 
+  setUIQuest(quest) {
+    return `
+      <div id="${quest.id}">
+        <div class="quest-info">
+          <h3 class="quest-title">${quest.title}</h3>
+          <p class="quest-title">${quest.description}</p>
+        </div>
+        <ul class="quest-tasks">
+          ${Object.values(quest.activeTasks).map((task) => {
+            return `
+              <li id="${task.id}" class="quest-task">
+                <span class="quest-task-name">${task.name}</span>
+                <span class="quest-task-goal">${task.goal.progress} / ${task.goal.objective}</span>
+              </li>`
+          }).join('')}
+          ${Object.values(quest.completedTasks).map((task) => {
+            return `
+              <li id="${task.id}" class="quest-task">
+                <span class="quest-task-name">${task.name}</span>
+                <span class="quest-task-goal">${task.goal.progress} / ${task.goal.objective}</span>
+              </li>`
+            }).join('')}
+        </ul>
+      </div> 
+    `
   }
 
   destroy() {
+    this.quest = null
+    this.experience.world.quest.off("update")
+  }
 
+  wait(callback) {
+    return new Promise((resolve) => {
+      const check = () => {
+        const value = callback();
+        if (value) {
+          resolve(value);
+        } else {
+          setTimeout(check, 0);
+        }
+      };
+      check();
+    });
   }
 }
