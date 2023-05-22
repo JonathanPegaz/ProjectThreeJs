13cedd379a3a95ca4724a68eb7a5c57b57dd3d68
Thomas Ghignon <thomas@everide.app>
2023-05-22 16:11:53 +0200

    🚀 ADD Escort task

---
+++ client/src/Experience/World/Quest/Task/Escort.js
@@ -0,0 +1,19 @@
+import Task from "./Task.js";
+
+export default class Escort extends Task {
+  constructor(param) {
+    super();
+
+    this.init(param)
+  }
+
+  init(param) {
+    super.init(param)
+
+
+  }
+
+  destroy() {
+    super.destroy();
+  }
+}
